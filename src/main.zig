f29ad1e3dc3ce2f2c841484df5d07b74b46f55a1
Peter Hellberg <peter@c7.se>
2024-08-27 19:34:52 +0200

    src: update

--- src/main.zig
+++ src/main.zig
@@ -4,12 +4,172 @@ const w4 = @import("w4");
 var prng = std.Random.DefaultPrng.init(0);
 const random = prng.random();
 
+const Game = struct {
+    scene: enum {
+        menu,
+        play,
+        over,
+
+        const Self = @This();
+
+        fn next(self: Self) Self {
+            return switch (self) {
+                .menu => .play,
+                .play => .over,
+                .over => .menu,
+            };
+        }
+    } = .play,
+
+    disk: Disk = .{ .starts = 0 },
+
+    dice: [5]Die = .{
+        die(1, 1, 60, 30, 30),
+        die(2, 33, 60, 30, 30),
+        die(3, 65, 60, 30, 30),
+        die(4, 97, 60, 30, 30),
+        die(5, 129, 60, 30, 30),
+    },
+
+    playPalette: [4]u32,
+    menuPalette: [4]u32,
+    overPalette: [4]u32,
+
+    playerCount: u3 = 1,
+
+    u: u3 = 0,
+
+    mouse: w4.Mouse = .{},
+    button: w4.Button = .{},
+
+    nextBtn: Rect = .{
+        .c = 0x21,
+        .x = 130,
+        .y = 148,
+        .width = 20,
+        .height = 10,
+    },
+
+    rollBtn: Rect = .{
+        .c = 0x34,
+        .x = 96,
+        .y = 125,
+        .width = 58,
+        .height = 30,
+    },
+
+    fn start(self: *Game) void {
+        w4.SYSTEM_FLAGS.* = w4.SYSTEM_HIDE_GAMEPAD_OVERLAY;
+
+        _ = self.disk.increment();
+
+        for (&self.dice) |*d| {
+            if (!d.h) d.startRoll();
+
+            for (0..(@intCast(self.disk.starts))) |_| {
+                _ = random.int(i32);
+            }
+        }
+    }
+
+    fn update(self: *Game) void {
+        self.mouse.update();
+        self.button.update();
+
+        switch (self.scene) {
+            .menu => self.menu(),
+            .play => self.play(),
+            .over => self.over(),
+        }
+    }
+
+    fn menu(self: *Game) void {
+        w4.palette(self.menuPalette);
+
+        if (self.button.released(0, w4.BUTTON_UP)) self.playerCount +|= 1;
+        if (self.button.released(0, w4.BUTTON_DOWN) and self.playerCount > 1) self.playerCount -|= 1;
+
+        if (self.nextBtn.contains(&self.mouse)) {
+            if (self.mouse.released(w4.MOUSE_LEFT)) self.next();
+
+            self.nextBtn.c = 0x22;
+        }
+
+        w4.color(0x13);
+        w4.text("-==[ 10K GAME ]==-", 8, 20);
+
+        w4.color(0x13);
+        sany(10, 140, .{ "Player Count:", self.playerCount });
+
+        self.nextBtn.draw();
+    }
+
+    fn play(self: *Game) void {
+        w4.palette(self.playPalette);
+
+        var held: usize = 0;
+
+        for (&self.dice) |*d| {
+            if (d.h) held += 1;
+        }
+
+        const shouldStartRoll =
+            self.button.released(0, w4.BUTTON_1) or
+            self.mouse.released(w4.MOUSE_RIGHT) or
+            (held < 5 and self.rollBtn.clicked(&self.mouse, w4.MOUSE_LEFT));
+
+        for (&self.dice) |*d| {
+            d.update(&self.mouse);
+
+            if (shouldStartRoll) {
+                if (!d.h) d.startRoll();
+            }
+
+            d.draw();
+        }
+
+        if (held < 5) {
+            self.rollBtn.draw();
+            w4.color(0x41);
+            sany(self.rollBtn.x + 4, self.rollBtn.y + 12, .{ "ROLL ", 5 - held });
+        }
+    }
+
+    fn over(self: *Game) void {
+        w4.palette(self.overPalette);
+
+        w4.color(0x13);
+        w4.text("OVER!", 10, 20);
+
+        if (self.nextBtn.contains(&self.mouse)) {
+            if (self.mouse.released(w4.MOUSE_LEFT)) self.next();
+
+            self.nextBtn.c = 0x22;
+        }
+
+        self.nextBtn.draw();
+    }
+
+    fn next(self: *Game) void {
+        const n = self.scene.next();
+
+        switch (n) {
+            .menu => {
+                self.playerCount = 1;
+            },
+            .play => {},
+            .over => {},
+        }
+
+        self.scene = n;
+    }
+};
+
 const Die = struct {
+    h: bool = false,
     n: u3 = 0,
     l: u5 = 0,
     r: Rect,
-
-    h: bool = false,
 
     fn startRoll(d: *Die) void {
         d.l = 31;
@@ -24,17 +184,12 @@ const Die = struct {
     }
 
     fn update(d: *Die, m: *w4.Mouse) void {
-        // if (d.r.clicked(m, w4.MOUSE_LEFT)) {
-        //     if (!d.h) d.startRoll();
-        // }
-
         if (d.r.clicked(m, w4.MOUSE_LEFT)) {
             d.toggle();
         }
 
         d.l -|= 1;
 
-        if (d.l > 0) d.roll();
         if (d.l > 0 and @mod(d.l, 2) == 0) d.roll();
     }
 
@@ -103,184 +258,6 @@ const Die = struct {
     }
 };
 
-fn rect(x: i32, y: i32, width: u32, height: u32) Rect {
-    return .{ .c = 0x32, .x = x, .y = y, .width = width, .height = height };
-}
-
-fn die(n: u3, x: i32, y: i32, width: u32, height: u32) Die {
-    return .{ .n = n, .r = rect(x, y, width, height) };
-}
-
-const Game = struct {
-    scene: enum {
-        menu,
-        play,
-        over,
-
-        const Self = @This();
-
-        fn next(self: Self) Self {
-            return switch (self) {
-                .menu => .play,
-                .play => .over,
-                .over => .menu,
-            };
-        }
-    } = .play,
-
-    disk: Disk = .{ .starts = 0 },
-
-    dice: [5]Die = .{
-        die(1, 1, 60, 30, 30),
-        die(2, 33, 60, 30, 30),
-        die(3, 65, 60, 30, 30),
-        die(4, 97, 60, 30, 30),
-        die(5, 129, 60, 30, 30),
-    },
-
-    playPalette: [4]u32,
-    menuPalette: [4]u32,
-    overPalette: [4]u32,
-
-    playerCount: u3 = 1,
-
-    u: u3 = 0,
-
-    mouse: w4.Mouse = .{},
-    button: w4.Button = .{},
-
-    nextBtn: Rect = .{
-        .c = 0x21,
-        .x = 130,
-        .y = 148,
-        .width = 20,
-        .height = 10,
-    },
-
-    rollBtn: Rect = .{
-        .c = 0x34,
-        .x = 106,
-        .y = 125,
-        .width = 48,
-        .height = 30,
-    },
-
-    fn start(self: *Game) void {
-        w4.SYSTEM_FLAGS.* = w4.SYSTEM_HIDE_GAMEPAD_OVERLAY;
-
-        _ = self.disk.increment();
-
-        for (&self.dice) |*d| {
-            if (!d.h) d.startRoll();
-
-            for (0..(@intCast(self.disk.starts))) |_| {
-                _ = random.int(i32);
-            }
-        }
-    }
-
-    fn update(self: *Game) void {
-        self.mouse.update();
-        self.button.update();
-
-        switch (self.scene) {
-            .menu => self.menu(),
-            .play => self.play(),
-            .over => self.over(),
-        }
-
-        // self.cursor();
-    }
-
-    fn cursor(self: *Game) void {
-        if (self.mouse.held(w4.MOUSE_LEFT)) {
-            w4.color(0x33);
-            w4.circle(self.mouse.x, self.mouse.y, 2);
-        }
-    }
-
-    fn menu(self: *Game) void {
-        w4.palette(self.menuPalette);
-
-        if (self.button.released(0, w4.BUTTON_UP)) self.playerCount +|= 1;
-        if (self.button.released(0, w4.BUTTON_DOWN) and self.playerCount > 1) self.playerCount -|= 1;
-
-        if (self.nextBtn.contains(&self.mouse)) {
-            if (self.mouse.released(w4.MOUSE_LEFT)) self.next();
-
-            self.nextBtn.c = 0x22;
-        }
-
-        w4.color(0x13);
-        w4.text("-==[ 10K GAME ]==-", 8, 20);
-
-        w4.color(0x13);
-        sany(10, 140, .{ "Player Count:", self.playerCount });
-
-        self.nextBtn.draw();
-    }
-
-    fn play(self: *Game) void {
-        w4.palette(self.playPalette);
-
-        // {
-        //     w4.color(0x13);
-        //     sany(10, 140, .{ "Player Count:", self.playerCount });
-        // if (self.nextBtn.contains(&self.mouse)) {
-        //     if (self.mouse.released(w4.MOUSE_LEFT)) self.next();
-
-        //     self.nextBtn.c = 0x22;
-        // }
-        // }
-
-        for (&self.dice) |*d| {
-            d.update(&self.mouse);
-
-            if (self.button.released(0, w4.BUTTON_1) or self.mouse.released(w4.MOUSE_RIGHT) or self.rollBtn.clicked(&self.mouse, w4.MOUSE_LEFT)) {
-                if (!d.h) d.startRoll();
-            }
-
-            d.draw();
-        }
-
-        self.rollBtn.draw();
-
-        w4.color(0x43);
-        w4.text("ROLL", self.rollBtn.x + 8, self.rollBtn.y + 12);
-
-        // self.nextBtn.draw();
-    }
-
-    fn over(self: *Game) void {
-        w4.palette(self.overPalette);
-
-        w4.color(0x13);
-        w4.text("OVER!", 10, 20);
-
-        if (self.nextBtn.contains(&self.mouse)) {
-            if (self.mouse.released(w4.MOUSE_LEFT)) self.next();
-
-            self.nextBtn.c = 0x22;
-        }
-
-        self.nextBtn.draw();
-    }
-
-    fn next(self: *Game) void {
-        const n = self.scene.next();
-
-        switch (n) {
-            .menu => {
-                self.playerCount = 1;
-            },
-            .play => {},
-            .over => {},
-        }
-
-        self.scene = n;
-    }
-};
-
 const Rect = struct {
     c: u16 = 0,
     x: i32,
@@ -333,8 +310,8 @@ const allocator = fba.allocator();
 // https://lospec.com/palette-list/pen-n-paper
 //
 const penNpaper = .{
-    0xe4dbba,
     0xa4929a,
+    0xe4dbba,
     0x4f3a54,
     0x260d1c,
 };
@@ -352,6 +329,14 @@ fn any(x: i32, y: i32, args: anytype) void {
 
 fn sany(x: i32, y: i32, args: anytype) void {
     format(x, y, "{s}{any}", args);
+}
+
+fn rect(x: i32, y: i32, width: u32, height: u32) Rect {
+    return .{ .c = 0x32, .x = x, .y = y, .width = width, .height = height };
+}
+
+fn die(n: u3, x: i32, y: i32, width: u32, height: u32) Die {
+    return .{ .n = n, .r = rect(x, y, width, height) };
 }
 
 const Disk = struct {
